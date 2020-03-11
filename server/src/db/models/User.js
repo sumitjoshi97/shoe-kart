f07521f60f2548aeba3bcbe80a0d4974ea16ea4b
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-11 17:38:59 +0530

    add user model

---
+++ server/src/db/models/User.js
@@ -0,0 +1,13 @@
+import { model, Schema } from 'mongoose'
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, min: 8 },
+  profilePic: String,
+  isAdmin: { type: Boolean, default: false }
+})
+
+const User = model('User', userSchema)
+
+export default User
