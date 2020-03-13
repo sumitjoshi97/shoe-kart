0ea13eb3d2d8221be9ed5223de7726b3787a69e2
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-13 16:22:12 +0530

    add apollo server

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,21 @@
+import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
+
+import resolvers from './graphql/resolvers'
+import typeDefs from './graphql/typeDefs'
+
+const app = express()
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => ({ req, res })
+})
+
+apolloServer.applyMiddleware({ app, path: '/graphql' })
 
 app.get('/', (req, res) => res.status(404).json({ "message": "success" }))
 
-const port = 5000 || process.env.PORT
+const PORT = 5000 || process.env.PORT
 
-app.listen(port, () => console.log(`server running at ${port}`))
+app.listen(PORT, () => console.log(`server running at ${PORT}`))
