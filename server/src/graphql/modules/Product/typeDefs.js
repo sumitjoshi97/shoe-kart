9d6f415ba03f2443f1fdbfb1c28a8fd3b8c8bc7a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-19 11:06:20 +0530

    add product typedefs

---
+++ server/src/graphql/modules/Product/typeDefs.js
@@ -0,0 +1,37 @@
+import { gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+  extend type Query {
+    products: [Product] @isAuth(requires: USER)
+    product(_id: String!): Product
+  }
+
+  extend type Mutation {
+    addProduct(
+      name: String!, 
+      image: [String!], 
+      category: String!, 
+      size: [Int], 
+      color: [String!], 
+      gender: String!, 
+      description: String!, 
+      price: Int!
+    ): Product @isAuth(requires: ADMIN)
+
+    removeProduct(_id: ID!): Product @isAuth(requires: ADMIN)
+  }
+
+  type Product {
+    _id: ID!,
+    name: String!
+    image: [String!]
+    category: String
+    size: [Int]
+    color: [String!]
+    gender: String!
+    description: String!
+    price: Int!
+  }
+`
+
+export default typeDefs
