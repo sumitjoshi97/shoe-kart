2f6a90f715fe48c5daf1358287bae8150092dadd
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-20 20:17:29 +0530

    add cart typedefs

---
+++ server/src/graphql/modules/Cart/typeDefs.js
@@ -0,0 +1,26 @@
+import { gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+  extend type Query {
+    cart: Cart! @isAuth(requires: USER)
+  }
+
+  extend type Mutation {
+    addToCart(productId: ID!): Cart @isAuth(requires: USER)
+    removeFromCart(productId: ID!): Cart @isAuth(requires: USER)
+  }
+
+  type CartItem {
+    product: Product!
+    quantity: Int!
+    price: Int!
+  }
+
+  type Cart {
+    _id: ID!
+    user: User!
+    items: [CartItem]
+  }
+`
+
+export default typeDefs
