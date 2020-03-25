4bf8885639a7f4b7f04d541e0ab4b0357f732d73
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-25 17:50:49 +0530

    prettify code

--- server/src/graphql/modules/Product/resolvers/Query/product.js
+++ server/src/graphql/modules/Product/resolvers/Query/product.js
@@ -1,4 +1,4 @@
-import Product from "../../../../../db/models/Product";
+import Product from '../../../../../db/models/Product'
 
 async function product(_, args) {
   return await Product.findById(args._id)
--- server/src/graphql/modules/Product/resolvers/Query/products.js
+++ server/src/graphql/modules/Product/resolvers/Query/products.js
@@ -1,7 +1,7 @@
 import Product from '../../../../../db/models/Product'
 
-const products = () => {
-  return Product.find({}).sort({ lastUpdated: -1 })
+async function products() {
+  return await Product.find({}).sort({ lastUpdated: -1 })
 }
 
 export default products
