fe1a0aa1fa9f97ff9c486c7d28a95522930879f8
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-26 15:04:53 +0530

    export userInfo resolvers

---
+++ server/src/graphql/modules/UserInfo/resolvers/index.js
@@ -0,0 +1,11 @@
+import * as Query from './Query'
+import * as Mutation from './Mutation'
+import UserInfo from './UserInfo'
+
+const resolvers = {
+  Query,
+  Mutation,
+  UserInfo,
+}
+
+export default resolvers
