0933d887e5cb874acd39944d93ee0cc2b9ba0e75
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-03-29 18:40:59 +0530

    add cart, order, userInfo to root schema

--- server/src/graphql/modules/index.js
+++ server/src/graphql/modules/index.js
@@ -1,10 +1,13 @@
 import Auth from './Auth'
+import Cart from './Cart'
+import Order from './Order'
 import Product from './Product'
+import UserInfo from './UserInfo'
 
 import makeExecutableSchemaFromModules from '../../utils/modules'
 
 const schema = makeExecutableSchemaFromModules({
-  modules: [Auth, Product]
+  modules: [Auth, Cart, Order, Product, UserInfo],
 })
 
 export default schema
