3c0463e5dc9bcdfcf58f9e98f093b799aa5ee12c
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-04 17:49:44 +0530

    add hero component

---
+++ client/src/pages/Home/Hero/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.scss'
+
+const Hero: React.FC = () => (
+  <div className="hero">
+    <div className="hero__info">
+      <h2 className="hero__info__header">Shoes that can lead you</h2>
+      <p className="hero__info__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nesciunt
+        minus eligendi? Inventore nobis tempore eius sed, ullam fugiat cum rem
+        reprehenderit aliquid soluta atque minima maxime officia culpa iure!
+      </p>
+
+      <Link className="hero__info__shop-btn round-btn" to="/products">
+        Shop now
+      </Link>
+    </div>
+  </div>
+)
+
+export default Hero
