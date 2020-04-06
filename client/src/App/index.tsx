2a08fe3f351af8e5b988989238eb1b6c85b596b8
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-06 18:56:35 +0530

    update routes & add header to app

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import Header from 'src/components/Header'
 import Home from 'src/pages/Home'
+import Products from 'src/pages/Products'
 
 const App: React.FC = () => (
-  <Switch>
-    <Route path="/" component={Home} />
-  </Switch>
+  <>
+    <Header />
+    <Switch>
+      <Route path="/products" component={Products} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </>
 )
 
 export default App
