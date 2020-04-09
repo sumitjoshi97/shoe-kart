a3e5473ad3bf564d294bb38ecc67971e7f346af3
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-09 23:06:42 +0530

    fix syntax error

--- server/src/graphql/modules/Cart/typeDefs.js
+++ server/src/graphql/modules/Cart/typeDefs.js
@@ -1,31 +1,31 @@
 import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
-  extend type Query {
-    cart: Cart! @isAuth(requires: USER)
-  }
+	extend type Query {
+		cart: Cart! @isAuth(requires: USER)
+	}
 
-  extend type Mutation {
-    addToCart(productId: ID!): Cart @isAuth(requires: USER)
-    removeFromCart(productId: ID!): Cart @isAuth(requires: USER)
-    updateCart(
-      productId: ID!, 
-      action: CartAction!, 
-      quantity: Int
-      ): Cart !isAuth(requires: USER)
-  }
+	extend type Mutation {
+		addToCart(productId: ID!): Cart @isAuth(requires: USER)
+		removeFromCart(productId: ID!): Cart @isAuth(requires: USER)
+		updateCart(
+			productId: ID!
+			action: CartAction!
+			quantity: Int
+		): Cart! @isAuth(requires: USER)
+	}
 
-  type CartItem {
-    product: Product!
-    quantity: Int!
-    price: Int!
-  }
+	type CartItem {
+		product: Product!
+		quantity: Int!
+		price: Int!
+	}
 
-  type Cart {
-    _id: ID!
-    user: User!
-    items: [CartItem]
-  }
+	type Cart {
+		_id: ID!
+		user: User!
+		items: [CartItem]
+	}
 `
 
 export default typeDefs
