e8cc3f744c07c3e44054bf935b013bc6d21f9a18
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-10 19:25:17 +0530

    replace prodcts router link to results

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 import Header from '../components/shared/Header'
 import Home from '../components/Home'
-import Products from '../components/Products'
+import Results from '../components/Results'
 
 const App: React.FC = () => (
   <>
     <Header />
     <Switch>
-      <Route path="/products" component={Products} />
+      <Route path="/results" component={Results} />
       <Route path="/" component={Home} />
     </Switch>
   </>
