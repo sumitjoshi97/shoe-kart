35f640493986748c26df9aa826374b108735b6e4
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-15 19:47:16 +0530

    add cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,24 @@
 import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
-
+import cors from 'cors'
 import getUser from './utils/context'
 import schema from './graphql/modules'
 
 const app = express()
+
+app.use(
+  cors({
+    origin: (origin, cb) => cb(null, true),
+    credentials: true,
+    preflightContinue: true,
+    exposedHeaders: [
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept',
+      'X-Password-Expired',
+    ],
+    optionsSuccessStatus: 200,
+  })
+)
 
 const apolloServer = new ApolloServer({
   schema,
