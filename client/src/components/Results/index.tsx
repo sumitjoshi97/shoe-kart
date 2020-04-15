fdf9c184c7f85510907970dc7f9a605e7a073ee1
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-15 19:43:58 +0530

    add provider to results page

--- client/src/components/Results/index.tsx
+++ client/src/components/Results/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ProductsProvider from './products-context'
 
+import LeftNav from './LeftNav'
+import Products from './Products'
 import './styles.scss'
 
 const Results: React.FC = () => {
-  return <div className="results">results</div>
+  return (
+    <div className="results">
+      <ProductsProvider>
+        <LeftNav />
+        <Products />
+      </ProductsProvider>
+    </div>
+  )
 }
 
 export default Results
