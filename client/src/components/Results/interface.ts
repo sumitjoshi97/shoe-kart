bbe215ce8cca8d50516a41f3fda8aba0c57a4373
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-15 19:42:46 +0530

    add products typescript interface

---
+++ client/src/components/Results/interface.ts
@@ -0,0 +1,32 @@
+export interface IProductProps {
+  id: string
+  name: string
+  image: string
+  price: string
+}
+
+export interface IProduct {
+  _id: string
+  name: string
+  image: string[]
+  category: string
+  size: number[]
+  color: string[]
+  gender: string
+  description: string
+  price: string
+  [key: string]: string[] | number[] | string
+}
+
+export interface ICategory {
+  name: string
+  count: number
+}
+
+export interface IState {
+  products: IProduct[]
+  activeCategory: string
+  activeFilterGender: string
+  activeFilterColors: string[]
+  activeFilterSizes: number[]
+}
