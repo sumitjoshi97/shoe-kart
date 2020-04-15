e95bcfa252656f34c1e4f2d046432ca90792217a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-15 19:49:31 +0530

    update apollo client

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,22 @@
-import ApolloClient from 'apollo-boost'
+// import ApolloClient from 'apollo-boost'
+
+// const client = new ApolloClient({
+//   uri: process.env.SERVER_URI + '/graphql',
+// })
+
+// export default client
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+export const cache = new InMemoryCache()
 
 const client = new ApolloClient({
-  uri: process.env.SERVER_URI + '/graphql',
+  cache,
+  link: new HttpLink({
+    // credentials: 'include',
+    uri: process.env.SERVER_URI + '/graphql',
+  }),
 })
 
 export default client
