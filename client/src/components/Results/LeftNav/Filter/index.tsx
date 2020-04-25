ae72ff8f5c00db4f434237bab7f05c0aeba975f2
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-25 22:40:08 +0530

    refactor

--- client/src/components/Results/LeftNav/Filter/index.tsx
+++ client/src/components/Results/LeftNav/Filter/index.tsx
@@ -3,24 +3,22 @@ import FilterItem from './FilterItem'
 
 interface IFilterProps {
   type: string
-  filterOptions: (type: string) => (string | number)[]
+  filterOptions: (string | number)[]
   activeFilterOptions: (string | number)[]
   dispatch: React.Dispatch<any>
 }
 
-const Filter: React.FC<IFilterProps> = ({
-  type,
-  filterOptions,
-  activeFilterOptions,
-  dispatch,
-}) => {
-  const renderFilters = (options: (string | number)[]) => {
+const Filter: React.FC<IFilterProps> = props => {
+  const { type, filterOptions, activeFilterOptions, dispatch } = props
+  const renderFilter = (options: (string | number)[]) => {
     return options.map((option: string | number, index: number) => (
       <FilterItem
         key={index + ' ' + option}
         name={option}
         type={type}
-        isFilterActive={activeFilterOptions.includes(option)}
+        isFilterActive={
+          activeFilterOptions && activeFilterOptions.includes(option)
+        }
         dispatch={dispatch}
       />
     ))
@@ -29,9 +27,7 @@ const Filter: React.FC<IFilterProps> = ({
   return (
     <div className="filter">
       <div className="filter__header">{type}</div>
-      <div className="filter__options">
-        {renderFilters(filterOptions(type))}
-      </div>
+      <div className="filter__options">{renderFilter(filterOptions)}</div>
     </div>
   )
 }
