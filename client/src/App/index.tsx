5bc33032d258eabab7960a0f2f6b277d86144459
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-27 19:00:16 +0530

    add product route

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -4,14 +4,18 @@ import { Route, Switch } from 'react-router-dom'
 import Header from '../components/shared/Header'
 import Home from '../components/Home'
 import Results from '../components/Results'
+import Product from '../components/Product'
 
 const App: React.FC = () => (
   <>
     <Header />
-    <Switch>
-      <Route path="/results" component={Results} />
-      <Route path="/" component={Home} />
-    </Switch>
+    <div className="app-container">
+      <Switch>
+        <Route path="/product/:productId" component={Product} />
+        <Route path="/results" component={Results} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
   </>
 )
 
