6f522f055d24f903676830fe47cdc46d7e8cb643
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-27 19:02:34 +0530

    add link to product

--- client/src/components/Results/Products/ProductCard.tsx
+++ client/src/components/Results/Products/ProductCard.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import { IProductProps } from '../interface'
 
 const ProductCard: React.FC<IProductProps> = props => {
-  const { name, image, price } = props
+  const { id, name, image, price } = props
 
   const product = (
     <div className="product-item">
+      <Link to={`product/${id}`} className="product-item__link" />
       <div className="product-item__img">
         <img src={image} alt={name} />
       </div>
