acf01f0641c75ad631d85fd294733848602be321
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-27 19:01:38 +0530

    add image-slider component for product page

---
+++ client/src/components/Product/ImageSlider.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+interface IImageSliderProps {
+  images: string[]
+  name: string
+}
+
+const ImageSlider: React.FC<IImageSliderProps> = ({ images, name }) => {
+  const [currentImage, setCurrentImage] = useState(images[0])
+
+  const currentOptionStyles = {
+    borderWidth: '2px',
+    borderStyle: 'solid',
+    borderColor: '#000',
+    borderRadius: '4px',
+  }
+
+  const handleSliderImage = (image: string) => {
+    setCurrentImage(image)
+  }
+
+  const renderSliderOptions = (images: string[]) => {
+    return images.map((image: string, index: number) => (
+      <div
+        className="slider-option"
+        key={index}
+        style={image === currentImage ? currentOptionStyles : undefined}
+        onClick={() => handleSliderImage(image)}
+      >
+        <img src={image} />
+      </div>
+    ))
+  }
+
+  return (
+    <div className="image-slider">
+      <div className="image-slider__nav">{renderSliderOptions(images)}</div>
+      <div className="image-slider__img-container">
+        <img
+          className="image-slider__img-container__curr-img"
+          src={currentImage}
+          alt={name}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ImageSlider
