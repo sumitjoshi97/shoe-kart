b316200db3a1acbfd1a53439153c5f094a0d6b9a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-04-29 20:22:41 +0530

    refactor: convert context to reducer hook

--- client/src/components/Results/hooks/resultsReducer.ts
+++ client/src/components/Results/hooks/resultsReducer.ts
@@ -1,20 +1,32 @@
-import React, { createContext, useReducer, useContext } from 'react'
-import { IState, IProduct, ICategory } from './interface'
+import { IResultsState, IProduct, ICategoryMap } from '../interface'
 
-interface category {
-  [key: string]: ICategory
+const actionTypes = {
+  SET_RESULTS_STORE: 'SET_RESULTS_STORE',
+  SET_ACTIVE_CATEGORY: 'SET_ACTIVE_CATEGORY',
+  SET_ACTIVE_FILTERS: 'SET_ACTIVE_FILTERS',
+  RESET_RESULTS_STORE: 'RESET_RESULTS_STORE',
+  TOGGLE_LEFT_NAV: 'TOGGLE_LEFT_NAV',
+  SET_SORT_BY: 'SET_SORT_BY',
 }
 
+const sortOptions = [
+  { title: 'Featured', value: '' },
+  { title: 'Price: High to Low', value: 'priceDesc' },
+  { title: 'Price: Low to High', value: 'priceAsc' },
+]
+
 const initialState = {
-  products: [],
   categories: [],
   activeCategory: '',
   filterTypes: ['gender', 'color', 'size'],
   filters: {},
   activeFilters: {},
+  showLeftNav: true,
+  sortOptions,
+  sortBy: {},
 }
 
-const getFilterOptions = (filterType: string, products: IProduct[]) => {
+const setFilterOptions = (filterType: string, products: IProduct[]) => {
   const filterOptions = products
     .map((product: IProduct) => product[filterType])
     .flat()
@@ -31,13 +43,20 @@ const getFilterOptions = (filterType: string, products: IProduct[]) => {
   return filterOptions
 }
 
-const setProductsStore = (state: IState, products: IProduct[]) => {
+const setInitialActiveFilters = () => {
+  return initialState.filterTypes.reduce((obj: any, filterType: string) => {
+    obj[filterType] = []
+    return obj
+  }, {})
+}
+
+const setResultsStore = (state: IResultsState, products: IProduct[]) => {
   const { filterTypes } = state
 
   const categories = Object.values(
     products
       .map((product: IProduct) => product.category)
-      .reduce((newCategories: category, category: string) => {
+      .reduce((newCategories: ICategoryMap, category: string) => {
         !newCategories[category]
           ? (newCategories[category] = { name: category, count: 1 })
           : (newCategories[category].count += 1)
@@ -46,19 +65,16 @@ const setProductsStore = (state: IState, products: IProduct[]) => {
   )
 
   const filters = filterTypes.reduce((obj: any, filterType: string) => {
-    obj[filterType] = getFilterOptions(filterType, products)
+    obj[filterType] = setFilterOptions(filterType, products)
     return obj
   }, {})
 
-  const activeFilters = filterTypes.reduce((obj: any, filterType: string) => {
-    obj[filterType] = []
-    return obj
-  }, {})
+  const activeFilters = setInitialActiveFilters()
 
-  return { ...state, products, categories, filters, activeFilters }
+  return { ...state, categories, filters, activeFilters }
 }
 
-const setActiveCategory = (state: IState, category: string) => {
+const setActiveCategory = (state: IResultsState, category: string) => {
   if (state.activeCategory !== category) {
     return { ...state, activeCategory: category }
   } else if (state.activeCategory === category) {
@@ -67,7 +83,7 @@ const setActiveCategory = (state: IState, category: string) => {
 }
 
 const setActiveFilters = (
-  state: IState,
+  state: IResultsState,
   filterType: string,
   filterOption: string | number,
 ) => {
@@ -87,7 +103,7 @@ const setActiveFilters = (
   }
 }
 
-const resetProducts = (state: IState) => {
+const resetResultsStore = (state: IResultsState) => {
   const activeFilters = state.filterTypes.reduce(
     (obj: any, filterType: string) => {
       obj[filterType] = []
@@ -102,55 +118,33 @@ const resetProducts = (state: IState) => {
   }
 }
 
-const reducer: React.Reducer<any, any> = (state: IState, action: any) => {
+//reducer for useResults reducer hook
+const resultsReducer: React.Reducer<any, any> = (
+  state: IResultsState,
+  action: any,
+) => {
   switch (action.type) {
-    case 'SET_PRODUCTS':
-      return setProductsStore(state, action.products)
+    case actionTypes.SET_RESULTS_STORE:
+      return setResultsStore(state, action.products)
 
-    case 'SET_ACTIVE_CATEGORY':
+    case actionTypes.SET_ACTIVE_CATEGORY:
       return setActiveCategory(state, action.category)
 
-    case 'SET_ACTIVE_FILTER':
+    case actionTypes.SET_ACTIVE_FILTERS:
       return setActiveFilters(state, action.filterType, action.filterOption)
 
-    case 'RESET_PRODUCTS':
-      return resetProducts(state)
+    case actionTypes.RESET_RESULTS_STORE:
+      return resetResultsStore(state)
+
+    case actionTypes.SET_SORT_BY:
+      return { ...state, sortBy: action.sortBy }
+
+    case actionTypes.TOGGLE_LEFT_NAV:
+      return { ...state, showLeftNav: !state.showLeftNav }
+
     default:
       return state
   }
 }
 
-const ProductsStateContext = createContext<any>(initialState)
-const ProductsDispatchContext = createContext<any>(initialState)
-
-const ProductsProvider = (props: any) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  return (
-    <ProductsStateContext.Provider value={{ state }}>
-      <ProductsDispatchContext.Provider value={{ dispatch }}>
-        {props.children}
-      </ProductsDispatchContext.Provider>
-    </ProductsStateContext.Provider>
-  )
-}
-
-const useProductState = () => {
-  const context = useContext(ProductsStateContext)
-
-  if (context === undefined) {
-    throw Error('useProductState must be used within ProductsProvider')
-  }
-  return context
-}
-
-const useProductDispatch = () => {
-  const context = useContext(ProductsDispatchContext)
-
-  if (context === undefined) {
-    throw Error('useProductDispatch must be used within ProductsProvider')
-  }
-  return context
-}
-
-export { useProductState, useProductDispatch, ProductsProvider }
+export { actionTypes, initialState, resultsReducer }
