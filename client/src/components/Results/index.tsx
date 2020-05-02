e3ee1558c90c2a32531375ac970fa101da440183
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-02 21:29:49 +0530

    add useResults hooks in results component

--- client/src/components/Results/index.tsx
+++ client/src/components/Results/index.tsx
@@ -1,17 +1,54 @@
-import React from 'react'
-import { ProductsProvider } from './products-context'
-
+import React, { useEffect } from 'react'
 import LeftNav from './LeftNav'
 import Products from './Products'
+import ResultsHeader from './ResultsHeader'
 import './styles.scss'
+import * as queries from './queries'
+import { useQuery } from 'react-apollo'
+import useResults from './hooks/useResults'
 
 const Results: React.FC = () => {
+  const { data } = useQuery(queries.getAllProducts)
+  const {
+    resultsStore,
+    setResultsStore,
+    setActiveCategory,
+    setActiveFilters,
+    resetResultsStore,
+    setSortBy,
+    toggleLeftNav,
+  } = useResults()
+
+  //intialize results-store with graphql data
+  useEffect(() => {
+    if (data) {
+      setResultsStore(data.products)
+    }
+  }, [data])
+
   return (
-    <div className="results">
-      <ProductsProvider>
-        <LeftNav />
-        <Products />
-      </ProductsProvider>
+    <div className="results-container">
+      <div className="results-header-offset" />
+      <ResultsHeader
+        category={resultsStore.activeCategory}
+        showLeftNav={resultsStore.showLeftNav}
+        sortOptions={resultsStore.sortOptions}
+        sortBy={resultsStore.sortBy}
+        setSortBy={setSortBy}
+        toggleLeftNav={toggleLeftNav}
+      />
+      <div className="results-header-offset" />
+      <div className="results">
+        {resultsStore.showLeftNav && (
+          <LeftNav
+            resultsStore={resultsStore}
+            setActiveCategory={setActiveCategory}
+            setActiveFilters={setActiveFilters}
+            resetResultsStore={resetResultsStore}
+          />
+        )}
+        <Products results={data} resultsStore={resultsStore} />
+      </div>
     </div>
   )
 }
