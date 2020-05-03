0beba65c14b7b70d74b56addc02e9a9b3e91565a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-03 20:32:44 +0530

    update args name

--- server/src/graphql/modules/Auth/resolvers/Mutation/signupAdmin.js
+++ server/src/graphql/modules/Auth/resolvers/Mutation/signupAdmin.js
@@ -4,24 +4,23 @@ import mongoose from 'mongoose'
 import { encodeToken } from '../../../../../utils/token'
 import User from '../../../../../db/models/User'
 
-async function signupAdmin(parent, args, context) {
-  const user = await User.findOne({ email: args.email })
-  if (!user) {
-    const _id = new mongoose.Types.ObjectId()
-    const password = await bcrypt.hash(args.password, 10)
-    const user = new User({ _id, ...args, password, isAdmin: true })
-    await user.save()
+async function signupAdmin(_, args) {
+	const user = await User.findOne({ email: args.email })
+	if (!user) {
+		const _id = new mongoose.Types.ObjectId()
+		const password = await bcrypt.hash(args.password, 10)
+		const user = new User({ _id, ...args, password, isAdmin: true })
+		await user.save()
 
-    const token = encodeToken(user._id)
+		const token = encodeToken(user._id)
 
-    return {
-      token,
-      user
-    }
-  }
-  else {
-    throw new Error('user exists')
-  }
+		return {
+			token,
+			user,
+		}
+	} else {
+		throw new Error('user exists')
+	}
 }
 
 export default signupAdmin
