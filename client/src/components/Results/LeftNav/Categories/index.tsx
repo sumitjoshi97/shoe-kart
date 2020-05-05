5db472511f60c4477d71973c2a56bc404517f857
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-05 16:58:22 +0530

    update props

--- client/src/components/Results/LeftNav/Categories/index.tsx
+++ client/src/components/Results/LeftNav/Categories/index.tsx
@@ -1,26 +1,19 @@
 import React from 'react'
 
 import CategoryItem from './CategoryItem'
-import { ICategory } from '../../interface'
-
-interface ICategoryProps {
-  categories: ICategory[]
-  activeCategory: string
-  dispatch: React.Dispatch<any>
-}
+import { ICategory, ICategoryProps } from '../../interface'
 
 const Categories: React.FC<ICategoryProps> = props => {
-  const { categories, activeCategory, dispatch } = props
+  const { categories, activeCategory } = props
 
   const renderCategories = (categories: ICategory[]) => {
     return categories.map((category: ICategory, index: number) => (
       <CategoryItem
         key={index + ' ' + category.name}
         name={category.name}
-        value={category.name}
         count={category.count}
         isActiveCategory={activeCategory === category.name}
-        dispatch={dispatch}
+        setActiveCategory={props.setActiveCategory}
       />
     ))
   }
