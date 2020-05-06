83c2f3a2a840123bc00d097970e225b490f04819
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-06 14:45:23 +0530

    refactor code

--- client/src/components/Results/Products/ProductCard.tsx
+++ client/src/components/Results/Products/ProductCard.tsx
@@ -4,17 +4,26 @@ import { Link } from 'react-router-dom'
 import { IProductProps } from '../interface'
 
 const ProductCard: React.FC<IProductProps> = props => {
-  const { id, name, image, price } = props
+  const { id, name, image, price, gender, category, length } = props
 
   const product = (
-    <div className="product-item">
-      <Link to={`product/${id}`} className="product-item__link" />
-      <div className="product-item__img">
+    <div className="product-card">
+      <Link to={`product/${id}`} className="product-card__link" />
+      <div
+        className="product-card__img"
+        style={{
+          width: `calc(${length}px - 2rem)`,
+          height: `calc(${length}px - 2rem)`,
+        }}
+      >
         <img src={image} alt={name} />
       </div>
-      <div className="product-item__info">
-        <div className="product-item__info__name">{name}</div>
-        <div className="product-item__info__price">{price}</div>
+      <div className="product-card__info">
+        <div className="product-card__info__name">{name}</div>
+        <div className="product-card__info__price">{price}</div>
+      </div>
+      <div className="product-card__type">
+        {gender}'s {category} shoe{' '}
       </div>
     </div>
   )
