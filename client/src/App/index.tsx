65c95eb14cdd6fbbab28c9c953bc69312127fa49
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-16 15:20:42 +0530

    refactor code

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-import Header from '../components/shared/Header'
+import Auth from '../components/Auth'
+import Header from '../components/Header'
 import Home from '../components/Home'
 import Results from '../components/Results'
 import Product from '../components/Product'
+import { useGlobalState } from '~store'
 
-const App: React.FC = () => (
-  <>
-    <Header />
-    <div className="app-container">
-      <Switch>
-        <Route path="/product/:productId" component={Product} />
-        <Route path="/results" component={Results} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </div>
-  </>
-)
+const App: React.FC = () => {
+  const {
+    state: { auth },
+  } = useGlobalState()
+
+  console.info('@@app', auth)
+  return (
+    <>
+      <Header />
+      <div className="app-container">
+        {auth.showAuthDialog && auth.userId === '' && <Auth />}
+        <Switch>
+          <Route path="/product/:productId" component={Product} />
+          <Route path="/results" component={Results} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </>
+  )
+}
 
 export default App
--- client/src/components/Results/LeftNav/Filter/index.tsx
+++ client/src/components/Results/LeftNav/Filter/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import FilterItem from './FilterItem'
-
-interface IFilterProps {
-  type: string
-  filterOptions: (string | number)[]
-  activeFilterOptions: (string | number)[]
-  dispatch: React.Dispatch<any>
-}
+import { IFilterProps } from '../../interface'
 
 const Filter: React.FC<IFilterProps> = props => {
-  const { type, filterOptions, activeFilterOptions, dispatch } = props
-  const renderFilter = (options: (string | number)[]) => {
-    return options.map((option: string | number, index: number) => (
+  const { type, filterOptions, activeFilterOptions } = props
+
+  const renderFilter = () => {
+    return filterOptions.map((option: string | number, index: number) => (
       <FilterItem
         key={index + ' ' + option}
         name={option}
@@ -19,7 +14,7 @@ const Filter: React.FC<IFilterProps> = props => {
         isFilterActive={
           activeFilterOptions && activeFilterOptions.includes(option)
         }
-        dispatch={dispatch}
+        setActiveFilters={props.setActiveFilters}
       />
     ))
   }
@@ -27,7 +22,7 @@ const Filter: React.FC<IFilterProps> = props => {
   return (
     <div className="filter">
       <div className="filter__header">{type}</div>
-      <div className="filter__options">{renderFilter(filterOptions)}</div>
+      <div className="filter__options">{renderFilter()}</div>
     </div>
   )
 }
