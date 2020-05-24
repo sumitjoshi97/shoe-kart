ba8d8b9fce315452824ad9ed231937d9f0e9bd86
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-24 22:33:43 +0530

    remove productInCart check

--- server/src/graphql/modules/Cart/resolvers/Mutation/addToCart.js
+++ server/src/graphql/modules/Cart/resolvers/Mutation/addToCart.js
@@ -2,42 +2,34 @@ import Cart from '../../../../../db/models/Cart'
 import Product from '../../../../../db/models/Product'
 
 async function addToCart(_, args, { user }) {
-  try {
-    const cart = await Cart.findOne({ user: user.userId })
-    const product = await Product.findById(args.productId)
+	try {
+		const cart = await Cart.findOne({ user: user.userId })
+		const product = await Product.findById(args.productId)
 
-    if (product) {
-      const newCartItem = {
-        product: args.productId,
-        quantity: 1,
-        price: product.price,
-      }
-      if (cart) {
-        const isProductInCart = cart.items.find(
-          item => String(item.product) === args.productId
-        )
-        if (!isProductInCart) {
-          cart.items.push(newCartItem)
-          const updatedCart = await cart.save()
-          return updatedCart
-        } else {
-          return new Error('product already added to cart')
-        }
-      }
+		if (product) {
+			const newCartItem = {
+				product: args.productId,
+				quantity: 1,
+				selectedSize: args.selectedSize,
+			}
+			if (cart) {
+				cart.items.push(newCartItem)
+				const updatedCart = await cart.save()
+				return updatedCart
+			}
 
-      const newCart = new Cart({
-        user: user.userId,
-        items: [newCartItem],
-      })
-      await newCart.save()
-      return newCart
-    } else {
-      throw new Error('product not avaiable')
-    }
-  } catch (err) {
-    console.log(err)
-    throw new Error('error occured')
-  }
+			const newCart = await new Cart({
+				user: user.userId,
+				items: [newCartItem],
+			}).save()
+			return newCart
+		} else {
+			throw new Error('product not avaiable')
+		}
+	} catch (err) {
+		console.log(err)
+		throw new Error('error occured')
+	}
 }
 
 export default addToCart
