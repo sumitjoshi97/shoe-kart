7dbadeeb46949d7008fc4c4121bc5c38b13a4a88
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-24 22:32:25 +0530

    prettify code

--- client/src/components/Product/queries.ts
+++ client/src/components/Product/queries.ts
@@ -3,14 +3,15 @@ import gql from 'graphql-tag'
 export const getProduct = gql`
   query($productId: String!) {
     product(productId: $productId) {
-    _id
-    name
-    image
-    category
-    size
-    color
-    gender
-    description
-    price}
+      _id
+      name
+      image
+      category
+      size
+      color
+      gender
+      description
+      price
+    }
   }
 `
