4b782de9598fc07cca56d173a2a7ad5554119fda
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-26 14:24:45 +0530

    refactor api authorization header

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -3,39 +3,16 @@ import ApolloClient from 'apollo-boost'
 const client = new ApolloClient({
   uri: process.env.SERVER_URI + '/graphql',
   request: operation => {
-    const token = localStorage.getItem('token')
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    })
+    const credentials = localStorage.getItem('credentials')
+    if (credentials) {
+      const { token_type, access_token } = JSON.parse(credentials)
+      operation.setContext({
+        headers: {
+          authorization: token_type + access_token,
+        },
+      })
+    }
   },
 })
 
 export default client
-// import { ApolloClient } from 'apollo-client'
-// import { setContext } from 'apollo-link-context'
-// import { HttpLink } from 'apollo-link-http'
-// import { InMemoryCache } from 'apollo-cache-inmemory'
-
-// export const cache = new InMemoryCache()
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('token')
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   }
-// })
-// const client = new ApolloClient({
-//   cache,
-//   link: new HttpLink({
-//     credentials: 'include',
-//     uri: process.env.SERVER_URI + '/graphql',
-//   }),
-// })
-
-// export default client
