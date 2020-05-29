a28891cfe74d1b93cb2a9de1c9e8bcf639e6b7de
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-29 15:20:43 +0530

    update constraints on quantity

--- server/src/db/models/Cart.js
+++ server/src/db/models/Cart.js
@@ -5,7 +5,7 @@ const cartSchema = new Schema({
   items: [
     {
       product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-      quantity: { type: Number, default: 1 },
+      quantity: { type: Number, default: 1, min: 1, max: 10},
       selectedSize: { type: Number, required: true },
     },
   ],
