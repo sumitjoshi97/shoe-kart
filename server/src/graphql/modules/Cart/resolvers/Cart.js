244acd55e7ce8590f412466798189fb0d2527c03
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-05-30 15:22:21 +0530

    add selected-size to return

--- server/src/graphql/modules/Cart/resolvers/Cart.js
+++ server/src/graphql/modules/Cart/resolvers/Cart.js
@@ -2,20 +2,21 @@ import Product from '../../../../db/models/Product'
 import User from '../../../../db/models/User'
 
 const Cart = {
-  items: async (cart) => {
-    const items = cart.items.map(item => {
-      const product = Product.findById(item.product)
-      const quantity = item.quantity
-      const price = item.price
-      return { product, quantity, price }
-    })
-    return items
-  },
+	items: async cart => {
+		const items = cart.items.map(item => {
+			const _id = item._id
+			const product = Product.findById(item.product)
+			const quantity = item.quantity
+			const selectedSize = item.selectedSize
+			return { _id, product, quantity, selectedSize }
+		})
+		return items
+	},
 
-  user: async (cart) => {
-    const user = await User.findById(cart.user)
-    return user
-  }
+	user: async cart => {
+		const user = await User.findById(cart.user)
+		return user
+	},
 }
 
 export default Cart
