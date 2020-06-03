33cd3f38000bab7788c0dc066ac481da512b5d88
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-06-03 11:39:34 +0530

    update cart resolver

--- server/src/graphql/modules/Cart/resolvers/Mutation/removeFromCart.js
+++ server/src/graphql/modules/Cart/resolvers/Mutation/removeFromCart.js
@@ -1,25 +1,23 @@
 import Cart from '../../../../../db/models/Cart'
-import Product from '../../../../../db/models/Product'
 
 async function removeFromCart(_, args, { user }) {
-  try {
-    const cart = await Cart.findOne({ user: user.userId })
-    const isProductInCart = cart.items.find(
-      item => item.product.toString() === args.productId
-    )
-    const product = await Product.findById(args.productId)
+	try {
+		const cart = await Cart.findOne({ user: user.userId })
+		const cartItem = cart.items.find(
+			item => item._id.toString() === args.cartItemId
+		)
 
-    if (product && cart && isProductInCart) {
-      const updatedCart = await Cart.findOneAndUpdate(
-        { user: user.userId },
-        { $pull: { items: { product: args.productId } } },
-        { new: true }
-      )
-      return updatedCart
-    }
-  } catch (err) {
-    throw new Error('cant remove product from cart, try again')
-  }
+		if (!cart && !cartItem) throw new Error()
+
+		const updatedCart = await Cart.findOneAndUpdate(
+			{ user: user.userId },
+			{ $pull: { items: { _id: args.cartItemId } } },
+			{ new: true }
+		)
+		return updatedCart
+	} catch (err) {
+		throw new Error('cant remove item from cart, try again')
+	}
 }
 
 export default removeFromCart
--- server/src/graphql/modules/Cart/resolvers/Query/cart.js
+++ server/src/graphql/modules/Cart/resolvers/Query/cart.js
@@ -1,19 +1,18 @@
 import Cart from '../../../../../db/models/Cart'
 
-async function cart(_, args, { user }) {
-  try {
-    const cart = await Cart.findOne({ user: user.userId })
-    //return cart items if cart exists
-    if (cart) {
-      return cart
-    }
-    //create new empty cart object if cart does not exist
-    cart = new Cart({ user: user.userId, cart: [] })
-    return cart
-  }
-  catch (err) {
-    throw new Error('something wrong happened try again')
-  }
+async function cart(_, __, { user }) {
+	try {
+		const cart = await Cart.findOne({ user: user.userId })
+		//return cart items if cart exists
+		if (cart) {
+			return cart
+		}
+		//create new empty cart object if cart does not exist
+		cart = new Cart({ user: user.userId, items: [] })
+		return cart
+	} catch (err) {
+		throw new Error('something wrong happened try again')
+	}
 }
 
 export default cart
