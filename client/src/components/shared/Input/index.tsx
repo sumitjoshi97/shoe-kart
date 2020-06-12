6a8bf369f7f7c555bda0ee8b483402ed94badc14
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-06-12 16:57:37 +0530

    move input to shared

---
+++ client/src/components/shared/Input/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './styles.scss'
+
+interface IInputProps {
+  type: string
+  label: string
+  name: string
+  inputValue: string
+  handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input: React.FC<IInputProps> = props => {
+  const { type, label, name, inputValue, handleInput } = props
+
+  const labelStyles = {
+    transform: 'translateY(0.7rem) scale(0.75)',
+  }
+
+  return (
+    <div className="input">
+      <input
+        className="input__field"
+        type={type}
+        name={name}
+        value={inputValue}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e)}
+        autoComplete="off"
+      />
+      <label
+        htmlFor={label}
+        className="input__label"
+        style={inputValue ? labelStyles : undefined}
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default Input
