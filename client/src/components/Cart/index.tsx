69ef7648bc767ad81fd015514bf992b01d53a01e
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-07-13 14:12:59 +0530

    add functions to update local cart

--- client/src/components/Cart/index.tsx
+++ client/src/components/Cart/index.tsx
@@ -3,13 +3,20 @@ import { useQuery, useMutation } from 'react-apollo'
 import * as queries from './queries'
 import CartItem from './CartItem'
 import './styles.scss'
+import { useGlobalState, useGlobalDispatch } from '~store'
+import isEmpty from '~helpers/isEmpty'
+import { getLocalCart } from '~helpers/localCart'
+import { ICartItem } from './interface'
 import OrderSummary from '../shared/CartSummary'
 import Button from '~components/shared/Button'
 import { Link } from 'react-router-dom'
 import { FiArrowRight } from 'react-icons/fi'
 
 const Cart = () => {
-  const { data } = useQuery(queries.GET_CART)
+  const { state } = useGlobalState()
+  const { dispatch } = useGlobalDispatch()
+
+  const { data } = useQuery(queries.GET_CART, { fetchPolicy: 'network-only' })
   const [updateCartItem] = useMutation(queries.UPDATE_CART_ITEM)
   const [removeCartItem] = useMutation(queries.REMOVE_FROM_CART)
 
@@ -18,59 +25,67 @@ const Cart = () => {
     quantity: number,
     selectedSize: number,
   ) => {
-    updateCartItem({
-      variables: { cartItemId, quantity, selectedSize },
-      refetchQueries: [{ query: queries.GET_CART }],
-    })
+    if (isEmpty(state.userId) && !isEmpty(state.cart)) {
+      dispatch({ type: 'UPDATE_CART_ITEM', cartItemId, quantity, selectedSize })
+      dispatch({ type: 'UPDATE_CART_INFO', cart: state.cart })
+    }
+    if (!isEmpty(state.userId)) {
+      updateCartItem({
+        variables: { cartItemId, quantity, selectedSize },
+        refetchQueries: [{ query: queries.GET_CART }],
+      })
+      dispatch({ type: 'UPDATE_CART_INFO', cart: data.cart.items })
+    }
   }
 
   const handleRemoveCartItem = (cartItemId: string) => {
-    removeCartItem({
-      variables: { cartItemId },
-      refetchQueries: [{ query: queries.GET_CART }],
-    })
+    if (isEmpty(state.userId)) {
+      dispatch({ type: 'REMOVE_FROM_CART', cartItemId })
+      dispatch({ type: 'UPDATE_CART_INFO', cart: data.cart.items })
+    }
+    if (!isEmpty(state.userId)) {
+      removeCartItem({
+        variables: { cartItemId },
+        refetchQueries: [{ query: queries.GET_CART }],
+      })
+      dispatch({ type: 'UPDATE_CART_INFO', cart: data.cart.items })
+    }
+  }
+
+  const getCartItems = () => {
+    if (data && data.cart && !isEmpty(state.userId)) {
+      return data.cart.items
+    }
+
+    if (isEmpty(state.userId) && !isEmpty(state.cart)) {
+      return Object.values(state.cart)
+    }
+
+    const cart = getLocalCart()
+    if (cart && isEmpty(state.userId) && isEmpty(state.cart)) {
+      const parsedCart = JSON.parse(cart)
+      if (!isEmpty(parsedCart)) {
+        dispatch({ type: 'INIT_CART', cart: parsedCart })
+        return Object.values(parsedCart)
+      }
+    }
+
+    return []
   }
 
   const renderCartItems = () => {
-    if (data && data.cart) {
-      return data.cart.items.map((cartItem: any) => {
-        const {
-          _id: cartItemId,
-          product: {
-            _id: productId,
-            name,
-            image,
-            price,
-            size,
-            gender,
-            category,
-          },
-          quantity,
-          selectedSize,
-        } = cartItem
-
-        return (
-          <CartItem
-            key={cartItemId}
-            id={cartItemId}
-            productId={productId}
-            name={name}
-            image={image[0]}
-            price={price}
-            size={size}
-            gender={gender}
-            category={category}
-            quantity={quantity}
-            selectedSize={selectedSize}
-            updateCartItem={handleCartItemUpdate}
-            removeCartItem={handleRemoveCartItem}
-          />
-        )
-      })
-    }
+    return getCartItems().map((cartItem: ICartItem) => (
+      <CartItem
+        key={cartItem._id}
+        cartItem={cartItem}
+        updateCartItem={handleCartItemUpdate}
+        removeCartItem={handleRemoveCartItem}
+      />
+    ))
   }
+
   return (
-      <div className="cart">
+    <div className="cart">
       <div className="cart__items">
         <h2 className="cart__items__header">My Bag</h2>
         <div className="cart__items__list">{renderCartItems()}</div>
