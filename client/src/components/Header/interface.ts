94eda9f11a60625a12ab993918f0d82ba8060e5d
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-07-17 22:11:46 +0530

    update header type interface

--- client/src/components/Header/interface.ts
+++ client/src/components/Header/interface.ts
@@ -1,3 +1,7 @@
+import { RouteComponentProps } from 'react-router-dom'
+
+export interface IHeaderProps extends RouteComponentProps {}
+
 export interface ISearchInputProps {
   searchText: string
   handleSearchInput: (e: React.ChangeEvent<HTMLInputElement>) => void
