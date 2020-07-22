19347c3fc366a2d9e00af0c9a1d58098e3f1257e
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-07-22 16:08:54 +0530

    update order mongo schema

--- server/src/db/models/Order.js
+++ server/src/db/models/Order.js
@@ -1,19 +1,15 @@
 import mongoose from 'mongoose'
 
 const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  items: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-      price: { type: Number, required: true },
-      quantity: { type: Number, default: 1 },
-      selectedSize: { type: Number, required: true }
-    },
-  ],
-  date: { type: Date, default: new Date() },
-  paymentType: String,
-  paymentStatus: String,
-  isOrderCompleted: { type: Boolean, default: false },
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	items: [
+		{
+			product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+			quantity: { type: Number, default: 1 },
+			selectedSize: { type: Number, required: true },
+		},
+	],
+	date: { type: Date, default: new Date() },
 })
 
 const Order = mongoose.model('Order', orderSchema)
