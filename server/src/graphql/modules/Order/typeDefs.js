b548a037f1c6483258dc5131aebde03bf339350f
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-07-31 15:41:41 +0530

    update orders graphql schema

--- server/src/graphql/modules/Order/typeDefs.js
+++ server/src/graphql/modules/Order/typeDefs.js
@@ -1,46 +1,39 @@
 import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
-  scalar Date
+	scalar Date
 
-  extend type Query {
-    order(orderId: ID!): Order!
-    orders: [Order]
-  }
+	extend type Query {
+		order(orderId: ID!): Order!
+		orders: [Order]
+	}
 
-  extend type Mutation {
-    addOrder(
-      items: [OrderItemInput!]
-      address: AddressInput!
-      paymentType: PaymentType!
-      paymentStatus: PaymentStatus!
-      isOrderCompleted: Boolean!
-    ): Order! @isAuth(requires: USER)
-  }
+	extend type Mutation {
+		createOrder(token: String!): Order! @isAuth(requires: USER)
+	}
 
-  type OrderItem {
-    _id: ID!
-    product: Product!
-    price: Int!
-    quantity: Int!
-  }
+	type OrderItem {
+		_id: ID!
+		product: Product!
+		quantity: Int!
+		selectedSize: Int!
+	}
 
-  type Order {
-    _id: ID!
-    user: User!
-    userInfo: UserInfo!
-    items: [OrderItem]
-    date: Date!
-    paymentType: PaymentType!
-    paymentStatus: PaymentStatus!
-    isOrderCompleted: Boolean!
-  }
+	type Order {
+		_id: ID!
+		user: User!
+		userInfo: UserInfo!
+		quantity: Int!
+		price: Int!
+		items: [OrderItem]
+		date: Date!
+	}
 
-  input OrderItemInput {
-    product: ID!
-    price: Int!
-    quantity: Int!
-  }
+	input OrderItemInput {
+		product: ID!
+		price: Int!
+		quantity: Int!
+	}
 `
 
 export default typeDefs
