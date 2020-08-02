a87e04f693fe0742258fd56d627115696e78aad6
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-08-02 14:44:00 +0530

    add paymentId property

--- server/src/db/models/User.js
+++ server/src/db/models/User.js
@@ -1,11 +1,12 @@
 import { model, Schema } from 'mongoose'
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, min: 8 },
-  profilePic: String,
-  isAdmin: { type: Boolean, default: false },
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true, min: 8 },
+	profilePic: String,
+	paymentId: String,
+	isAdmin: { type: Boolean, default: false },
 })
 
 const User = model('User', userSchema)
