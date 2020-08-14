8f1afc3df74125f4da6257fe1a6f64bc0cec4b9a
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-08-14 15:47:07 +0530

    update product typedefs as per category schema

--- server/src/graphql/modules/Product/typeDefs.js
+++ server/src/graphql/modules/Product/typeDefs.js
@@ -1,37 +1,33 @@
 import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
-  extend type Query {
-    products: [Product]
-    product(productId: String!): Product
-  }
+	extend type Query {
+		products(main_category: ID, categories: [ID]): [Product]
+		product(productId: ID!): Product
+	}
 
-  extend type Mutation {
-    addProduct(
-      name: String!
-      image: [String!]
-      category: String!
-      size: [Int]
-      color: [String!]
-      gender: String!
-      description: String!
-      price: Int!
-    ): Product @isAuth(requires: ADMIN)
+	extend type Mutation {
+		addProduct(
+			name: String!
+			image: [String!]
+			main_category: ID!
+			categories: [ID]!
+			description: String!
+			price: Int!
+		): Product @isAuth(requires: ADMIN)
 
-    removeProduct(_id: ID!): Product @isAuth(requires: ADMIN)
-  }
+		removeProduct(_id: ID!): Product @isAuth(requires: ADMIN)
+	}
 
-  type Product {
-    _id: ID!
-    name: String!
-    image: [String!]
-    category: String
-    size: [Int]
-    color: [String!]
-    gender: String!
-    description: String!
-    price: Int!
-  }
+	type Product {
+		_id: ID!
+		name: String!
+		image: [String!]
+		main_category: Category!
+		categories: [Category!]!
+		description: String!
+		price: Int!
+	}
 `
 
 export default typeDefs
