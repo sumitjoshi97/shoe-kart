540addeda5e00a5614483f4dce392e7b6c1c8c26
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-09-22 15:15:31 +0530

    remove charge functionality from create order

--- server/src/graphql/modules/Order/resolvers/Mutation/createOrder.js
+++ server/src/graphql/modules/Order/resolvers/Mutation/createOrder.js
@@ -1,41 +1,10 @@
 import Order from '../../../../../db/models/Order'
 import Cart from '../../../../../db/models/Cart'
-import User from '../../../../../db/models/User'
-import { stripe } from '../../../../../utils/stripe'
 
-async function getCustomerPaymentId(user, source) {
-	if (!user.paymentId) {
-		const customer = await stripe.customers.create({
-			email: user.email,
-			source,
-			name: user.name,
-		})
-
-		user.paymentId = customer.id
-		await user.save()
-		return customer.id
-	}
-
-	return user.paymentId
-}
-
-async function createOrder(_, { token }, { user }) {
+async function createOrder(_, __, { user }) {
 	try {
 		const cart = await Cart.findOne({ user: user.userId })
-		const currentUser = await User.findById(user.userId)
-
 		if (!cart && cart.items.length === 0) throw new Error('empty cart')
-
-		const charge = await stripe.charges.create({
-			amount: cart.price * 100,
-			currency: 'inr',
-			description: 'items bought',
-			customer: await getCustomerPaymentId(currentUser, token),
-		})
-
-		if (!charge) {
-			throw new Error('payment unsuccessful try again')
-		}
 
 		const newOrder = new Order({
 			user: user.userId,
