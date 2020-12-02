a255c3f6337550a8c7407c59776c6e791acc7af2
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-12-02 14:10:23 +0530

    add payment methods to context

--- client/src/context/UIContext.tsx
+++ client/src/context/UIContext.tsx
@@ -1,16 +1,17 @@
 import React, { createContext, useState } from 'react'
-import { ISortOption } from '~interface'
 
 export interface IUIContext {
   activeMainCategory: string
   activeFilterCategories: string[]
+  activePaymentMethod: string
   showDialog: boolean
   showLeftNav: boolean
-  sortBy: ISortOption
+  sortBy: string | undefined
   handleActiveMainCategory: (category: string) => void
   handleActiveFilterCategories: (category: string) => void
   resetCategories: () => void
-  setSortBy: React.Dispatch<React.SetStateAction<ISortOption>>
+  handleActivePaymentMethod: (paymentMethodId: string) => void
+  handleSortBy: (sortOption: string) => void
   toggleDialog: () => void
   toggleLeftNav: () => void
 }
@@ -18,13 +19,14 @@ export interface IUIContext {
 export const UIContext = createContext({} as IUIContext)
 
 export const UIProvider: React.FC<React.ReactNode> = ({ children }) => {
-  const [sortBy, setSortBy] = useState<ISortOption | {}>({})
+  const [sortBy, setSortBy] = useState<string>()
   const [showDialog, setShowDialog] = useState<boolean>(false)
   const [showLeftNav, setShowLeftNav] = useState<boolean>(true)
   const [activeMainCategory, setActiveMainCategory] = useState<string>('')
   const [activeFilterCategories, setActiveFilterCategories] = useState<
     string[]
   >([])
+  const [activePaymentMethod, setActivePaymentMethod] = useState<string>('')
 
   const handleActiveMainCategory = (category: string) => {
     if (!!activeMainCategory && activeMainCategory === category) {
@@ -44,6 +46,14 @@ export const UIProvider: React.FC<React.ReactNode> = ({ children }) => {
     }
   }
 
+  const handleActivePaymentMethod = (paymentMethodId: string) => {
+    setActivePaymentMethod(paymentMethodId)
+  }
+
+  const handleSortBy = (sortOption: string) => {
+    setSortBy(sortOption)
+  }
+
   const resetCategories = () => {
     setActiveMainCategory('')
     setActiveFilterCategories([])
@@ -61,13 +71,15 @@ export const UIProvider: React.FC<React.ReactNode> = ({ children }) => {
       value={{
         activeMainCategory,
         activeFilterCategories,
+        activePaymentMethod,
         showDialog,
         showLeftNav,
         sortBy,
         handleActiveMainCategory,
         handleActiveFilterCategories,
         resetCategories,
-        setSortBy,
+        handleActivePaymentMethod,
+        handleSortBy,
         toggleDialog,
         toggleLeftNav,
       }}
