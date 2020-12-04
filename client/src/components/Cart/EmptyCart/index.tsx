122a0c4fce512b5c4d7cd56e9e79a16ea67dd160
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-12-04 15:59:01 +0530

    move EmptyCart to directory

--- client/src/components/Cart/EmptyCart/index.tsx
+++ client/src/components/Cart/EmptyCart/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { FiShoppingBag } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import Button from '../shared/Button'
-import './styles.scss'
+
+import Button from '~components/shared/Button'
 
 const EmptyCart = () => {
   return (
-    <div className="empty-cart medium-container">
+    <div className="empty-cart">
       <FiShoppingBag
         style={{ marginBottom: '2rem', height: '12rem', width: '12rem' }}
       />
-      <h2 className="title-primary">Sorry, it seems something is empty here</h2>
+      <h2>Sorry, it seems something is empty here</h2>
       <p className="empty-cart__text">
         It appears that your cart is empty, don't be afraid of spending here. We
         already got your credit card.
