ef9e65742a25a9eb4ddbda17a2f9c9cb7c8751c8
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-12-07 18:25:45 +0530

    add disabled value to Input component

--- client/src/components/shared/Input/index.tsx
+++ client/src/components/shared/Input/index.tsx
@@ -5,27 +5,46 @@ interface IInputProps {
   type: string
   label: string
   name: string
-  inputValue: string
+  inputValue?: string
+  disabled?: boolean
   handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const Input: React.FC<IInputProps> = props => {
-  const { type, label, name, inputValue, handleInput } = props
+  const { type, label, name, inputValue, disabled, handleInput } = props
 
   const labelStyles = {
-    transform: 'translateY(0.4rem) scale(0.75)',
+    transform: 'scale(0.8) translateY(0.4rem)',
   }
 
-  return (
-    <div className="input">
+  const renderInputElement = () => {
+    if (disabled) {
+      return (
+        <input
+          className="input__field"
+          type={type}
+          name={name}
+          disabled={disabled}
+          defaultValue={inputValue}
+        />
+      )
+    }
+    return (
       <input
         className="input__field"
         type={type}
         name={name}
+        disabled={disabled}
         value={inputValue}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInput(e)}
         autoComplete="off"
       />
+    )
+  }
+
+  return (
+    <div className="input">
+      {renderInputElement()}
       <label
         htmlFor={label}
         className="input__label"
@@ -39,3 +58,7 @@ const Input: React.FC<IInputProps> = props => {
 }
 
 export default Input
+
+Input.defaultProps = {
+  disabled: false,
+}
