86fe0ae5b3ade1b27869f2d5bf0815aa694e4e7d
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-12-07 18:16:13 +0530

    refactor root component

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,65 +1,39 @@
 import React, { Suspense, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
+
 import Auth from '../components/Auth'
 import Header from '../components/Header'
-import { useGlobalState, useGlobalDispatch } from '~store'
-import routes from './routes'
-import isEmpty from '~helpers/isEmpty'
-import { ADD_TO_CART } from '../components/Cart/queries'
-import { getLocalCart } from '~helpers/localCart'
-import { useMutation } from '@apollo/react-hooks'
 
-const App: React.FC = () => {
-  const { state } = useGlobalState()
-  const { dispatch } = useGlobalDispatch()
+import routes from './routes'
+import useStores from '~hooks/useStores'
+
+const App = () => {
+  const { authStore, uiStore } = useStores()
+  const { addUser, isAuth } = authStore
+  const { showDialog } = uiStore
 
   useEffect(() => {
     const credentials = localStorage.getItem('credentials')
     if (credentials) {
       const userId = JSON.parse(credentials).user_id
-      dispatch({ type: 'ADD_USER', user: userId })
+      const token = JSON.parse(credentials).token
+      addUser(userId, token)
     }
   }, [])
-
-  useEffect(() => {
-    const cart = getLocalCart()
-    if (cart && isEmpty(state.userId)) {
-      const parsedCart = JSON.parse(cart)
-      if (!isEmpty(parsedCart)) {
-        dispatch({ type: 'INIT_CART', cart: parsedCart })
-        dispatch({ type: 'UPDATE_CART_INFO', cart: parsedCart })
-      }
-    }
-  }, [])
-
-  const [addToCart] = useMutation(ADD_TO_CART)
-
-  if (!isEmpty(state.userId) && !isEmpty(state.cart)) {
-    Object.values(state.cart).forEach((cartItem: any) =>
-      addToCart({
-        variables: {
-          productId: cartItem.product._id,
-          quantity: cartItem.quantity,
-          selectedSize: cartItem.selectedSize,
-        },
-      }),
-    )
-    dispatch({ type: 'CLEAR_CART' })
-  }
 
   return (
     <>
       <Header />
       <div className="app-container">
-        {state.toggleDialog && state.userId === '' && <Auth />}
+        {showDialog && !isAuth && <Auth />}
         <Switch>
           {routes.map(route => (
             <Route
               key={route.path}
               path={route.path}
-              render={() => (
+              render={props => (
                 <Suspense fallback={<div />}>
-                  <route.component />
+                  <route.component {...props} />
                 </Suspense>
               )}
             />
