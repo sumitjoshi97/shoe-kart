316f1ba98040afd81b43c7d76684ca00f60503d8
sumitjoshi97 <sumitjoshi479@gmail.com>
2020-12-11 14:31:47 +0530

    fix orders are not sorted by date

--- server/src/graphql/modules/Order/resolvers/Query/orders.js
+++ server/src/graphql/modules/Order/resolvers/Query/orders.js
@@ -1,7 +1,7 @@
 import Order from '../../../../../db/models/Order'
 
 async function orders(_, __, { user }) {
-  return await Order.find({ user: user.userId })
+	return await Order.find({ user: user.userId }).sort({ lastUpdated: -1 })
 }
 
 export default orders
